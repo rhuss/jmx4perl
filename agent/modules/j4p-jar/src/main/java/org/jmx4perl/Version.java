971273fde280d3549d6446b698382dc591bf3d81
Roland Huß <roland@tichny.org>
2009-07-06 11:06:11 +0200

    fixed permission issue when running 'Build dist'

--- agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
+++ agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
@@ -31,7 +31,7 @@ package org.jmx4perl;
  * @since Jun 11, 2009
  */
 public class Version {
-    private static String VERSION = "0.21_1";
+    private static String VERSION = "0.22_1";
 
     public static String getVersion() {
         return VERSION;
