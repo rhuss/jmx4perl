525fed00893f06dc1bd8b72bfc4e1d3ea38c3ef5
Roland Huß <roland@tichny.org>
2009-08-07 18:29:55 +0200

    Released 0.35_1

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "0.30";
+$VERSION = "0.35_1";
 
 my $REGISTRY = {
                 # Agent based
