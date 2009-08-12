33af3b9df991f6b2e4155ac19a2366d6fb1e94f9
Roland Huß <roland@tichny.org>
2009-08-12 21:23:31 +0200

    Updated CHANGES

--- agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
+++ agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
@@ -31,7 +31,7 @@ package org.jmx4perl;
  * @since Jun 11, 2009
  */
 public class Version {
-    private static String VERSION = "0.35_2";
+    private static String VERSION = "0.35_3";
 
     public static String getVersion() {
         return VERSION;
