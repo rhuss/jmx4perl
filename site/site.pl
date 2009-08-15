6bd8ae88c3c14c0e9fd889b8e79a0d68ef0d602d
Roland Huß <roland@tichny.org>
2009-08-15 09:31:39 +0200

    worked on

--- site/site.pl
+++ site/site.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 
 use FindBin qw($Bin);
+use lib qw(lib);
 use Config::General;
 use Cwd qw(realpath);
 use File::Path;
@@ -11,6 +12,9 @@ use Data::Dumper;
 use Pod::Simple::Search;
 use Pod::POM;
 use Pod::POM::View::HTML;
+use Template;
+
+use JMX::Jmx4Perl::Site::PodHtml;
 
 my $config = 
   { new Config::General(-f "$Bin/site_local.cfg" ? "$Bin/site_local.cfg" : "$Bin/site.cfg")->getall };
@@ -36,18 +40,49 @@ mkdir "$Bin/target";
 
 sub make_pods {
     print ":::: Making POD documentation\n";
+    my $target = "$Bin/target/pod";
+    mkdir $target;
+
     my $pod_search = new Pod::Simple::Search()->limit_glob("JMX::*");
     my $n2p = $pod_search->survey(realpath("$Bin/../lib"));
-    print Dumper($n2p);
-    my $t = (%$n2p)[3];
-    my $pom = Pod::POM->new();
-    $pom->parse($t);
+    my $pod = Pod::POM->new();
+    my $template = Template->new
+      ({
+        INCLUDE_PATH => "$Bin/template:$Bin/template/inc",
+        INTERPOLATE => 1,
+        DEBUG => 1
+       })
+      || die Tempate->error(),"\n";    
+    my $tt_args = 
+    { 
+     css_base_url => "../css/",
+     top_navigation => [
+                      { label => "Home", link => "../index.html" },
+                      { label => "Documentation", link => "../doc/index.html", selected => 1}
+                       ],
+     sub_navigation => [
+                       { label => "Manual", link => "../doc/manual/index.html" },
+                       { label => "Modules", link => "../doc/modules/index.html", selected => 1}
+                        ],
+    };
+    for my $name (keys %$n2p) {
+        my $pom = $pod->parse_file($n2p->{$name});
+        my $pod_html = $pom->present('JMX::Jmx4Perl::Site::PodHtml');
+        $template->process("main.tt",{ %$tt_args, content => $pod_html }, "$target/" . &module2filename($name) . ".html")
+          || die $template->error,"\n";
+    }
     #print Dumper($pod);
-    print $pom->present('Pod::POM::View::HTML');
+    #print $pom->present('Pod::POM::View::HTML');
 
+    #print $text,"\n";
     #print Dumper(new Pod::Simple::Search()->limit_glob("Pod::*")->survey());
 }
 
+sub module2filename {
+    my $mod = shift;
+    $mod =~ s/::/_/g;
+    return $mod;
+}
 sub make_blog {
 
 }
@@ -59,3 +94,7 @@ sub make_index {
 sub deploy {
 
 }
+
+__END__
+    my $text = "";
+    print ">>>> ",(%$n2p)[3],"\n";
