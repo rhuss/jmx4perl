b13d6435e3886da880080e7dc68311e982b857eb
Roland Huß <roland@tichny.org>
2009-10-22 14:11:11 +0200

    Prepared for release 0.36_1 + some ant/mvn build fixes

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "0.35";
+$VERSION = "0.36_1";
 
 my $REGISTRY = {
                 # Agent based
