e63b83e50145a87d8b83407becfd07d0e0e634d0
Roland Huß <roland@tichny.org>
2009-12-02 22:00:19 +0100

    prepared for 0.50_2

--- agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
+++ agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
@@ -32,7 +32,7 @@ package org.jmx4perl;
  */
 public final class Version {
 
-    private static final String VERSION = "0.50_1";
+    private static final String VERSION = "0.50_2";
 
     private Version() {}
 
