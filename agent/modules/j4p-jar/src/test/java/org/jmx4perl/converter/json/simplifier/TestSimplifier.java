4ab69ab334cdc0cafae790e41eacd2c9d92ac1f7
Roland Huß <roland@tichny.org>
2010-07-02 19:12:45 +0200

    Ignore help class for tests.

--- agent/modules/j4p-jar/src/test/java/org/jmx4perl/converter/json/simplifier/TestSimplifier.java
+++ agent/modules/j4p-jar/src/test/java/org/jmx4perl/converter/json/simplifier/TestSimplifier.java
@@ -3,10 +3,13 @@ package org.jmx4perl.converter.json.simplifier;
 import java.util.Date;
 import java.util.Map;
 
+import org.junit.Ignore;
+
 /**
  * @author roland
  * @since Jul 2, 2010
  */
+@Ignore
 public class TestSimplifier extends SimplifierExtractor<Date> {
     public TestSimplifier() {
         super(Date.class);
