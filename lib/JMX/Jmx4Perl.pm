cc0b41336546e7c18c7f20195e2c3c13dd4973da
Roland Huß <roland@cpan.org>
2011-08-21 17:26:58 +0200

    Version 0.95 prepared.

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "0.95_1";
+$VERSION = "0.95";
 
 my $REGISTRY = {
                 # Agent based
