64275f52999f1142cf903adfb585b6c143fcb23e
Roland Huß <roland@cpan.org>
2012-06-10 17:02:54 +0200

    Bumped up version

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "1.05";
+$VERSION = "1.06_1";
 
 my $REGISTRY = {
                 # Agent based
