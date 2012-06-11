d32ebd5ebd65626e07078af530d58084c0c9516b
Roland Huß <roland@cpan.org>
2012-06-11 10:38:05 +0200

    Added missing file

--- lib/JMX/Jmx4Perl.pm
+++ lib/JMX/Jmx4Perl.pm
@@ -101,7 +101,7 @@ use vars qw($VERSION $HANDLER_BASE_PACKAGE @PRODUCT_HANDLER_ORDERING);
 use Data::Dumper;
 use Module::Find;
 
-$VERSION = "1.06_1";
+$VERSION = "1.06_2";
 
 my $REGISTRY = {
                 # Agent based
